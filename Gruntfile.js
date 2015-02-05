c5a3f6d14d96aac4681c7f3a9fb90eaced7507d5
Dan Purdy <purdz0@gmail.com>
2015-02-05 23:49:47 +0000

    Add a grunt dev task
    Adds a manually run task that avoids sass linting so you can debug stuff without having to remove scss lint from the grunt file. Manual at the moment as you don't want all your changes compiling on every file change.

--- Gruntfile.js
+++ Gruntfile.js
@@ -186,6 +186,9 @@
         // Do Sass related tasks
         grunt.registerTask('do-sass', ['scsslint', 'sass:development', 'autoprefixer:all', 'notify:sass', 'do-ie']);
 
+        // Run a dev task for all options except sass lint
+        grunt.registerTask('dev', ['do-img', 'do-js', 'sass:development', 'autoprefixer:all', 'notify:sass', 'do-ie'] )
+
         // Do IE8 Legacy specific tasks
         grunt.registerTask('do-ie', ['pixrem']);
 
