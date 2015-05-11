13507a6a20641a6138add7eb3ed692b054e2f66a
Ben Griffith <ben@loftdigital.com>
2015-05-11 21:59:29 +0100

    Updated true intergrate with a node test runner - works with mocha - returns results in cli

---
+++ app/tests/scss/test.js
@@ -0,0 +1,15 @@
+
+
+(function () {
+  'use strict';
+
+  var path = require('path'),
+    truesass = require('sass-true');
+
+  var sassFile = path.join(__dirname, 'test.scss');
+
+  console.log(sassFile);
+
+  truesass.runSass({file: sassFile}, describe, it);
+
+})();
